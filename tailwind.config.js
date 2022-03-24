913f5c79e6782830be32fee3fcb45d8cdff02e05
Shilpa Bhattar <shilpab@exponentialai.com>
2022-03-24 18:28:59 +0530

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	purge: [
+		'./src/components/**/*.js',
+		'./pages/**/*.js'],
+	theme: {
+		extend: {},
+		minHeight: {
+			'60vh': '60vh'
+		}
+	},
+	variants: {},
+	plugins: [
+		require( 'tailwindcss' ),
+		require( 'precss' ),
+		require( 'autoprefixer' )
+	]
+}
